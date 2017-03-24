cfc54fa23782ccaaee82797db9ff16c19d28b6ad
polonie <lander89@inbox.ru>
2017-03-25 04:29:43 +0600

    .

--- public/js/findGroups.js
+++ public/js/findGroups.js
@@ -7,7 +7,7 @@ for(i=0; i<potentialGroups.length; i++){
 			currentBoutique.classList.add(textFields[j].innerHTML);
 		};
 		console.log(currentBoutique.className);
-		currentBoutique.setAttribute('data-boutique', toString(currentBoutique.className.baseVal));
+		currentBoutique.setAttribute('data-boutique', currentBoutique.classList);
 		currentBoutique.classList.add('boutique');
 	}
 };
