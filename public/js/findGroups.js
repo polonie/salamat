b2e81a2a3d83c81fe018cb9699b44b1cc3ba0312
polonie <lander89@inbox.ru>
2017-03-25 04:16:36 +0600

    .

--- public/js/findGroups.js
+++ public/js/findGroups.js
@@ -6,6 +6,7 @@ for(i=0; i<potentialGroups.length; i++){
 		for(j=0; j<textFields.length; j++){
 			currentBoutique.classList.add(textFields[j].innerHTML);
 		};
+		console.log(currentBoutique.className);
 		currentBoutique.setAttribute('data-boutique', toString(currentBoutique.className));
 		currentBoutique.className = 'boutique';
 	}
