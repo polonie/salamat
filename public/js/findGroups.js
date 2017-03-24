60d8ee166eddc8fe652ebfc2be96973050a16058
polonie <lander89@inbox.ru>
2017-03-25 03:36:35 +0600

    .

--- public/js/findGroups.js
+++ public/js/findGroups.js
@@ -3,6 +3,7 @@ for(i=0; i<potentialGroups.length; i++){
 	if(!(potentialGroups[i].firstChild.matches('.st5'))){
 		var currentBoutique = potentialGroups[i];
 		currentBoutique.classList.add('boutique');
-		currentBoutique.setAttribute('data-boutique', currentBoutique.querySelector('.st5').innerText);
+		var text = currentBoutique.querySelector('.st5').innerHTML;
+		currentBoutique.setAttribute('data-boutique', text);
 	}
 };
