30ec742254de3a654e829a38a378e7a2c3202ab0
polonie <lander89@inbox.ru>
2017-03-25 04:08:31 +0600

    .

--- public/js/findGroups.js
+++ public/js/findGroups.js
@@ -2,8 +2,11 @@ var potentialGroups = document.getElementsByTagName('g');
 for(i=0; i<potentialGroups.length; i++){
 	if(!(potentialGroups[i].firstChild.matches('.st5'))){
 		var currentBoutique = potentialGroups[i];
-		currentBoutique.classList.add('boutique');
-		var text = currentBoutique.querySelector('.st5').innerHTML;
-		currentBoutique.setAttribute('data-boutique', text);
+		var textFields = currentBoutique.querySelectorAll('.st5');
+		for(j=0; j<textFields.length; j++){
+			currentBoutique.classList.add(textFields[i].innerHTML);
+		};
+		currentBoutique.setAttribute('data-boutique', currentBoutique.className);
+		currentBoutique.className = 'boutique';
 	}
 };
