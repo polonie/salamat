034c47cfa37465457ebbea965daf79d046fae9f2
polonie <lander89@inbox.ru>
2017-03-27 01:03:30 +0600

    .

--- public/js/findBoutique.js
+++ public/js/findBoutique.js
@@ -1,18 +1,18 @@
 var ready = ()=>{
-	var boutiqueShow = document.querySelector('.boutique-show');
-	boutiqueShow.onclick = (e)=>{
+	var boutiqueCollection = document.querySelector('.boutiques-block-items');
+	boutiqueCollection.onclick = (e)=>{
 		var item = e.target;
-		console.log('salem');
-		while(item != boutiqueShow){
+		while(!item.matches('.boutique-show')){
 			item = item.parentElement;
 		};
-		console.log(item);
-		var data_salamat = item.getAttribute('data-salamat');
-		var data_salon = item.getAttribute('data-salon');
-		if (data_salon && data_salamat){
-			window.location.href = `/findboutique?data_salamat=${data_salamat}&data_salon=${data_salon}`;
+		if (item.matches('.boutique-show')){
+			var data_salamat = item.getAttribute('data-salamat');
+			var data_salon = item.getAttribute('data-salon');
+			if (data_salon && data_salamat){
+				window.location.href = `/findboutique?data_salamat=${data_salamat}&data_salon=${data_salon}`;
+			};
 		}else{
-			console.log(data_salon);
+			return;
 		}
 	};
 }
