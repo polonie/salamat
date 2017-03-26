da7336499148ed9a21dbd2eac143e430358c77ac
polonie <lander89@inbox.ru>
2017-03-26 23:17:40 +0600

    .

--- public/js/findBoutique.js
+++ public/js/findBoutique.js
@@ -3,10 +3,10 @@ var boutiqueShow = document.querySelector('.boutique-show');
 
 boutiqueShow.onclick = (e)=>{
 	var item = e.target;
-	var data_salamat = item.getAttribute('data-salamat');
+	// var data_salamat = item.getAttribute('data-salamat');
 	var data_salon = item.getAttribute('data-salon');
-	if (data_salamat && data_salon){
-		window.location.href = `/findboutique?data_salamat=${data_salamat}&data_salon=${data_salon}`;
+	if (data_salon){
+		window.location.href = `/findboutique?data_salon=${data_salon}`;
 	}else{
 		console.log(data_salamat, data_salon);
 	}
