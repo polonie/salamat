40a880316a84dae87f00bd7de724ef30b6d37339
polonie <lander89@inbox.ru>
2017-03-26 23:22:58 +0600

    .

--- public/js/findBoutique.js
+++ public/js/findBoutique.js
@@ -3,6 +3,7 @@ var boutiqueShow = document.querySelector('.boutique-show');
 
 boutiqueShow.onclick = (e)=>{
 	var item = e.target;
+	console.log(e.target);
 	// var data_salamat = item.getAttribute('data-salamat');
 	var data_salon = item.getAttribute('data-salon');
 	if (data_salon){
