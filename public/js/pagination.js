90444feae812ba07f0e3ed41f44aea0a8818e7cb
polonie <lander89@inbox.ru>
2017-03-30 22:51:28 +0600

    .

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,15 +1,13 @@
 var ready = ()=>{
 	var pages = document.querySelector('.pages');
 	var list = document.querySelector('.boutiques-block-items');
-	var pagesList = document.querySelectorAll('.page-item');
 	var indx = +(list.getAttribute('data-category')) - 1;
-	pagesList[indx].classList.add('active-page');
+	pages.children[indx].classList.add('active-page');
 
 	pages.onclick = (e)=>{
 		if (e.target.matches('.page-item')){
 			var pageNumb = e.target.getAttribute('data-page');
 			window.location.href = `/boutiques?pagenumber=${pageNumb}`;
-
 		};
 	};
 };
