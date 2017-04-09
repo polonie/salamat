331fb35ac395fad77de12f387951ddc167c273d2
polonie <lander89@inbox.ru>
2017-04-10 00:55:15 +0600

    .

--- public/js/newplan.js
+++ public/js/newplan.js
@@ -1,13 +1,19 @@
 var hoverBlock = document.querySelector('.hover-block');
 var currentImg = document.querySelector('.current-img');
 var currentSm = document.querySelector('.current-sm');
+var currentLink = document.querySelector('.current-link')
 
 document.onclick = (e)=>{
 	if (e.target.matches('.rect')){
 		hoverBlock.style.display = 'block';
-		var salamat_numb = e.target.parentElement.getAttribute('data-sm');
-		var salamat_img = e.target.parentElement.getAttribute('data-salamat');
+		var element = e.target.parentElement;
+		var salamat_numb = element.getAttribute('data-sm');
+		var salamat_img = element.getAttribute('data-salamat');
+		var salamat_link = element.getAttribute('data-smfloor');
 		currentImg.src = `./img/${salamat_img}.jpg`;
 		currentSm.innerHTML = `Саламат ${salamat_numb}`;
-	};
+		currentLink.href=`${salamat_link}`;
+	}else if(e.target.matches('.close-button')){
+		hoverBlock.style.display = 'none';
+	}
 };
