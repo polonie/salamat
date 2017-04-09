f0b6b8611d1d6406686237bfac0ed0d1498509b2
polonie <lander89@inbox.ru>
2017-04-09 19:31:04 +0600

    .

--- public/js/sendemail.js
+++ public/js/sendemail.js
@@ -12,21 +12,25 @@ button.onclick = (e)=>{
 	if (val1 && val2 && val3){
 		form_message.classList.remove('form-message-error');
 		form_message.classList.add('form-message-success');
-		form_message.style.opacity = '1';
-		form_message.innerText = 'Спасибо, ваши данные отправлены!';
-		setTimeout(()=>{
-			form_message.style.opacity = '0';
-		}, 1100);
-		// emailjs.send("mail_ru","template_4cwfGNrw", {name: val1, email: val2, text: val3})
-		// .then(function(response) {
-		// 	form_message.innerText = 'Спасибо, ваши данные отправлены!'
-		// 	form_message.classList.add('form-message-success');
-		// 	form_message.classList.remove('form-message-success');
-		// 	form_message.innerText = '';
-		// 	console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-		// }, function(err) {
-		// 	console.log("FAILED. error=", err);
-		// });
+		// form_message.style.opacity = '1';
+		// form_message.innerText = 'Спасибо, ваши данные отправлены!';
+		// setTimeout(()=>{
+		// 	form_message.style.opacity = '0';
+		// }, 1100);
+		emailjs.send("mail_ru","template_4cwfGNrw", {name: val1, email: val2, text: val3})
+		.then(function(response) {
+			form_message.innerText = 'Спасибо, ваши данные отправлены!'
+			form_message.classList.remove('form-message-error');
+			form_message.classList.add('form-message-success');
+			form_message.style.opacity = '1';
+			// form_message.innerText = '';
+			setTimeout(()=>{
+				form_message.style.opacity = '0';
+			}, 1100);
+			console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+		}, function(err) {
+			console.log("FAILED. error=", err);
+		});
 	}else{
 		console.log('Has not been sent');
 		form_message.classList.remove('form-message-success');
