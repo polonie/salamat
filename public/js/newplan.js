ac33204db325e217adb9feea1dd93071474f0100
polonie <lander89@inbox.ru>
2017-04-10 00:18:27 +0600

    .

--- public/js/newplan.js
+++ public/js/newplan.js
@@ -3,10 +3,10 @@ var currentImg = document.querySelector('.current-img');
 var currentSm = document.querySelector('.current-sm');
 
 document.onclick = (e)=>{
-	if (e.target.matches('.salamat')){
+	if (e.target.matches('.rect')){
 		hoverBlock.style.display = 'block';
-		var salamat_numb = e.target.getAttribute('data-sm');
-		var salamat_img = e.target.getAttribute('data-salamat');
+		var salamat_numb = e.target.parentElement.getAttribute('data-sm');
+		var salamat_img = e.target.parentElement.getAttribute('data-salamat');
 		currentImg.src = `./img/${salamat_img}.jpg`;
 		currentSm.innerHTML = `Саламат ${salamat_numb}`;
 	};
