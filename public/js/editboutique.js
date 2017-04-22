7c5a1fba08db8e2fc3eb3cc0eefe0c1720d9de0f
polonie <lander89@inbox.ru>
2017-04-23 01:27:36 +0600

    .

--- public/js/editboutique.js
+++ public/js/editboutique.js
@@ -6,7 +6,7 @@ form.onclick = function(e) {
 	console.log(this);
 	if (e.target.matches('.edit-button')){
 		const edit = e.target;
-		edit.prepreviousElementSibling.disabled = false;
-		edit.prepreviousElementSibling.focus();
+		edit.previousElementSibling.disabled = false;
+		edit.previousElementSibling.focus();
 	}
 };
