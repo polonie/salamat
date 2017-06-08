eafd21eba8f39c89aaaf0774aeac2b1223eddcbc
polonie <lander89@inbox.ru>
2017-06-08 17:00:33 +0600

    .

--- public/js/sendemail.js
+++ public/js/sendemail.js
@@ -5,20 +5,23 @@ var form_textarea = document.querySelector('.form-textarea');
 var form_message = document.querySelector('.form-message');
 form_message.style.opacity = '0';
 
-button.onclick = (e)=>{
+
+button.addEventListener('click', sendEmail);
+
+
+var sendEmail = (e)=>{
 	var val1 = form_input_name.value;
 	var val2 = form_input_email.value;
 	var val3 = form_textarea.value;
 	if (val1 && val2 && val3){
+		button.removeEventListener('click', sendEmail);
+		button.classList.add('form-button-inactive');
 		form_message.classList.remove('form-message-error');
 		form_message.classList.add('form-message-success');
-		// form_message.style.opacity = '1';
-		// form_message.innerText = 'Спасибо, ваши данные отправлены!';
-		// setTimeout(()=>{
-		// 	form_message.style.opacity = '0';
-		// }, 1100);
 		emailjs.send("mail_ru","template_4cwfGNrw", {name: val1, email: val2, text: val3})
 		.then(function(response) {
+			button.addEventListener('click', sendEmail);
+			button.classList.remove('form-button-inactive');
 			form_message.innerText = 'Спасибо, ваши данные отправлены!'
 			form_message.classList.remove('form-message-error');
 			form_message.classList.add('form-message-success');
