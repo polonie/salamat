ddcf352a5828c4f44931e79fee518cc1a9f4d7e1
polonie <lander89@inbox.ru>
2017-06-12 01:14:59 +0600

    .

--- public/js/delete-article.js
+++ public/js/delete-article.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
-	$('.delete-article').on('click', function(e) {
-		$target = $(e.target);
-		const id = $target.attr('data-id');
-		console.log(id);
-		$.ajax({
-			type: 'DELETE',
-			url: '/article/' + id,
-			success: function(response) {
-				window.location.href = '/articles';
-			},
-			error: function(err) {
-				console.log(err);
-			}
-		});
-	});
-});
+console.log('salem');
+// $(document).ready(function() {
+// 	$('.delete-article').on('click', function(e) {
+// 		$target = $(e.target);
+// 		const id = $target.attr('data-id');
+// 		console.log(id);
+// 		$.ajax({
+// 			type: 'DELETE',
+// 			url: '/article/' + id,
+// 			success: function(response) {
+// 				window.location.href = '/articles';
+// 			},
+// 			error: function(err) {
+// 				console.log(err);
+// 			}
+// 		});
+// 	});
+// });
