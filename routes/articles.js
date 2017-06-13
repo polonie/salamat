9da975cf35e02cd5b17692616a4008e482720532
polonie <lander89@inbox.ru>
2017-06-14 04:29:07 +0600

    .

--- routes/articles.js
+++ routes/articles.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 
 router.get('/', (req, res)=>{
-	Article.find({}, (err, articles)=>{
+	Article.find({}, sort({created: -1}).exec((err, articles)=>{
 		res.render('articles', {articles, page: 'list'})
-	});
+	}));
 });
 
 router.get('/add', ensureAuthenticated, (req, res)=>{
