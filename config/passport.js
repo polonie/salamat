a60f3f4701f04304bee9c4897e91d4c3f19f6d12
polonie <lander89@inbox.ru>
2017-06-14 00:07:32 +0600

    .

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/User');
+const {User} = require('../models/User');
 const config = require('../config/database');
 const bcrypt = require('bcryptjs');
 
